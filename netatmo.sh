88a29d05e54be60cdc6c67e488e583d556cf7442
busshi973 <busshi973@icloud.com>
2023-11-04 12:40:04 +0100

    change thermostat mode

--- netatmo.sh
+++ netatmo.sh
@@ -5,31 +5,51 @@ TOKEN_URL="https://api.netatmo.com/oauth2/token"
 
 
 refresh_token() {
-	refresh_token=$( domo_api.sh get_var_value "netatmo_token" )
+	refresh_token=$( domo_api.sh get_var_value "netatmo_refresh" )
 
-	req=$( curl -s -X POST -d "grant_type=refresh_token" -d "refresh_token=${refresh_token}" -d "client_id=${NETATMO_CLIENT_ID}" -d "client_secret=${NETATMO_CLIENT_SECRET}" "$TOKEN_URL" )
+	req=$( curl -s -d "grant_type=refresh_token" -d "refresh_token=${refresh_token}" -d "client_id=${NETATMO_CLIENT_ID}" -d "client_secret=${NETATMO_CLIENT_SECRET}" "$TOKEN_URL" )
 
 	new_refresh_token=$( echo "$req" | jq -r .refresh_token )
 	new_token=$( echo "$req" | jq -r .access_token )
 
-	# update refresh token in db
-	domo_api.sh update_var_value "netatmo_refresh" "$new_refresh_token" 2
-	domo_api.sh update_var_Value "netatmo_token" "$new_token" 2
+	if [ -n "$new_refresh_token" -a -n "$new_token" ]; then
+		# update refresh token in db
+		domo_api.sh update_var_value "netatmo_refresh" "$new_refresh_token" 2
+		domo_api.sh update_var_value "netatmo_api" "$new_token" 2
+		echo "Variables updated!"
+	else
+		echo "Error: $req"
+		exit 1
+	fi
+}
+
+get_token() {
+	token=$( domo_api.sh get_var_value "netatmo_api" )
 }
 
 switch_thermostat_schedule() {
-	token=$( domo_api.sh get_var_value "netatmo_token" )
+	get_token
+	req=$(curl -s -X POST "${API_URL}/switchhomeschedule?home_id=${NETATMO_HOME_ID}&schedule_id=${schedule_id}" -H "accept: application/json" -H "Authorization: Bearer ${token}")
+[ "$( echo $req | jq -r .status )" = "ok" ] && echo "[ OK ] Switched $1." || { refresh_token; switch_thermostat_schedule "$1"; }
+}
 
-	req=$(curl -X POST "${API_URL}/switchhomeschedule?home_id=${NETATMO_HOME_ID}&schedule_id=${schedule_id}" -H "accept: application/json" -H "Authorization: Bearer ${token}")
-[ "$( echo $req | jq -r .status )" = "ok" ] && echo "[ OK ]" || { refresh_token; switch_thermostat_schedule; }
+set_thermostat_mode() {
+	get_token
+        req=$(curl -s -X POST "${API_URL}/setthermmode?home_id=${NETATMO_HOME_ID}&mode=${therm_mode}" -H "accept: application/json" -H "Authorization: Bearer ${token}")
+[ "$( echo $req | jq -r .status )" = "ok" ] && echo "[ OK ] Switched $1." || { refresh_token; set_thermostat_mode "$1"; }
 }
 
 case "$1" in
 	"away")
-			schedule_id=${NETATMO_AWAY_SCHEDULE_ID}
+			#schedule_id=${NETATMO_AWAY_SCHEDULE_ID}
+			therm_mode="away"
 			;;
 	"home")
-			schedule_id=${NETATMO_HOME_SCHEDULE_ID}
+			#schedule_id=${NETATMO_HOME_SCHEDULE_ID}
+			therm_mode="schedule"
+			;;
+	"hg")
+			therm_mode="hg"
 			;;
 	*)
 			echo "Usage: ./netatmo.sh [home | away]"
@@ -37,7 +57,7 @@ case "$1" in
 			;;
 esac
 
-switch_thermostat_schedule
-
+#switch_thermostat_schedule
+set_thermostat_mode "$1"
 
 exit 0
